a6530685ea42cdd32eed21313315a2ac94493e10
Ivan Rodionov <uhf_job@mail.ru>
2023-05-16 21:01:19 +0300

    Add User can sign up

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User can sign up', %q{
+  In order to end session
+  As an authenticated user
+  I'd like to sign out
+} do
+
+  scenario 'Registered user tries to sign out' do
+  #   visit destroy_user_session_path
+
+  #   save_and_open_page
+  #   expect(page).to have_content 'signing out'
+  end
+end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'User can sign up', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to enter my details data then sign in
+} do
+  background { visit new_user_registration_path }
+
+  scenario 'Unregistered user tries to sign up' do
+    fill_in 'Email', with: attributes_for(:user)[:email]
+    fill_in 'Password', with: attributes_for(:user)[:password]
+    fill_in 'Password confirmation', with: attributes_for(:user)[:password]
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'Unregistered user tries to sign up with errors' do
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Password can't be blank"
+  end
+end
