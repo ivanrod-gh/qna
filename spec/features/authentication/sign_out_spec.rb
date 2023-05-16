1394fb210bb5c0a03bd1dcaac9b51205f9903ebd
Ivan Rodionov <uhf_job@mail.ru>
2023-05-16 21:37:07 +0300

    Add User can sign out

---
+++ spec/features/authentication/sign_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to end session
+  As an authenticated user
+  I'd like to sign out
+} do
+  given(:user) { create(:user) }
+
+  scenario 'Registered user tries to sign out' do
+    sign_in(user)
+
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
--- spec/features/authentication/sign_up_spec.rb
+++ spec/features/authentication/sign_up_spec.rb
@@ -11,13 +11,13 @@ feature 'User can sign up', %q{
     fill_in 'Email', with: attributes_for(:user)[:email]
     fill_in 'Password', with: attributes_for(:user)[:password]
     fill_in 'Password confirmation', with: attributes_for(:user)[:password]
-    click_on 'Sign up'
+    click_on 'Register'
 
     expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
   scenario 'Unregistered user tries to sign up with errors' do
-    click_on 'Sign up'
+    click_on 'Register'
 
     expect(page).to have_content "Email can't be blank"
     expect(page).to have_content "Password can't be blank"
