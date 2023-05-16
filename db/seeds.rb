aed468bb7fd0ed9d8e05eb05d04dfa5235aea897
Ivan Rodionov <uhf_job@mail.ru>
2023-05-16 15:26:07 +0300

    Add questions in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+Question.create!(title: 't1', body: 'b1')
+Question.create!(title: 't2', body: 'b2')
+Question.create!(title: 't3', body: 'b3')
