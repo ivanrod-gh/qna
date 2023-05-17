1aeb2fbef234b0ff98a987017dbc72e6588722d9
Ivan Rodionov <uhf_job@mail.ru>
2023-05-17 13:40:08 +0300

    Add User can watch question with answers

--- spec/features/questions/create_spec.rb
+++ spec/features/questions/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User can create question', %q{
-  In order to get answer from a comminuty
+  In order to get answer from a community
   As an authenticated user
   I'd like to be able to ask the question
 } do
