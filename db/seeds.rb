e560ecda755813c6112ec1991335d615d1f9cbb4
Ivan Rodionov <uhf_job@mail.ru>
2023-05-17 14:53:51 +0300

    Add User can move from viewing questions list to view current question and vice versa

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,6 @@
 Question.create!(title: 't1', body: 'b1')
 Question.create!(title: 't2', body: 'b2')
 Question.create!(title: 't3', body: 'b3')
+Question.first.answers.push(Answer.new(body: 'a1'))
+Question.first.answers.push(Answer.new(body: 'a2'))
+Question.first.answers.push(Answer.new(body: 'a3'))
