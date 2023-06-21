41e9badac2517c9310cccf0db896281f8ad2ad62
Ivan Rodionov <uhf_job@mail.ru>
2023-06-22 01:11:06 +0300

    Add Yandex as third party authentication provider

--- spec/features/requests/auth_spec.rb
+++ spec/features/requests/auth_spec.rb
@@ -4,29 +4,52 @@ feature 'User can be authenticated from third party website', %q{
   In order to get fast access to questions and answers
   As an unauthenticated user
   I'd like to be able to authenticate from another website
-}
+} do
+  describe "Unauthenticated user" do
+    describe 'tries to sign in throw GitHub' do
+      scenario "with valid auth data" do
+        mock_auth_github_valid_hash
 
-describe "Unauthenticated user" do
-  describe 'tries to sign in throw GitHub' do
-    scenario "with valid auth data" do
-      mock_auth_github_valid_hash
+        visit root_path
+        click_on 'Sign in'
+        click_on "Sign in with GitHub"
 
-      visit root_path
-      click_on 'Sign in'
-      click_on "Sign in with GitHub"
+        expect(page).to have_content "Successfully authenticated from Github account."
+        expect(page).to have_content "Sign out"
+      end
 
-      expect(page).to have_content "Successfully authenticated from GitHub account."
-      expect(page).to have_content "Sign out"
+      scenario "with invalid auth data" do
+        mock_auth_github_invalid_hash
+
+        visit root_path
+        click_on 'Sign in'
+        click_on "Sign in with GitHub"
+
+        expect(page).to have_content 'Authentication failed.'
+      end
     end
 
-    scenario "with invalid auth data" do
-      mock_auth_github_invalid_hash
+    describe 'tries to sign in throw Yandex' do
+      scenario "with valid auth data" do
+        mock_auth_yandex_valid_hash
 
-      visit root_path
-      click_on 'Sign in'
-      click_on "Sign in with GitHub"
+        visit root_path
+        click_on 'Sign in'
+        click_on "Sign in with Yandex"
 
-      expect(page).to have_content 'Authentication failed.'
+        expect(page).to have_content "Successfully authenticated from Yandex account."
+        expect(page).to have_content "Sign out"
+      end
+
+      scenario "with invalid auth data" do
+        mock_auth_yandex_invalid_hash
+
+        visit root_path
+        click_on 'Sign in'
+        click_on "Sign in with Yandex"
+
+        expect(page).to have_content 'Authentication failed.'
+      end
     end
   end
 end
