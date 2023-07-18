a7a35437f0c85be453a10ba4a6a8d4d66661b960
Ivan Rodionov <uhf_job@mail.ru>
2023-07-18 13:47:07 +0300

    Add gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: '95.163.242.214' }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials[:production][:gmail][:login],
+    password: Rails.application.credentials[:production][:gmail][:app_password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
